e0bc65977d3b9de13258382cbac4f45665bd43cb
trevyn <trevyn-git@protonmail.com>
2021-10-02 13:01:05 -0400

    get file list

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use nom::bytes::streaming::take;
 use nom::number::streaming::{be_u32, be_u64};
 use nom::IResult;
 use std::convert::TryInto;
+use std::ffi::{CStr, CString};
+use std::os::raw::{c_char, c_longlong, c_uchar};
+
+#[allow(dead_code, non_snake_case)]
+extern "C" {
+ fn GoListJSON(path: *const c_char);
+ fn GoFetchFiledata(path: *const c_char, startbytepos: c_longlong, endbytepos: c_longlong);
+}
 
 #[derive(Debug)]
 struct Box {
@@ -24,12 +32,61 @@ impl Box {
  }
 }
 
+/// Receive an array of File entries from Go and insert into turbosql
+/// # Safety
+/// `json` must be a valid pointer to valid C string until this function returns.
+#[no_mangle]
+extern "C" fn rust_insert_files_from_go(json: *const c_char) {
+ let c_str = unsafe { CStr::from_ptr(json) };
+ let string = c_str.to_str().unwrap().to_owned();
+ dbg!(string);
+
+ // let mut sender = RESPONSE_TX_CHANNEL.lock().unwrap().clone().unwrap();
+
+ // tokio::spawn(async move {
+ //  sender.send(string).await.unwrap();
+ // });
+}
+
+/// Receive a Filecache entry from Go and insert into turbosql
+/// buf is only valid until function return, must be copied
+#[no_mangle]
+extern "C" fn rust_insert_filecache_from_go(
+ json: *const c_char,
+ buf: *const c_uchar,
+ len: c_longlong,
+) {
+ let c_str = unsafe { CStr::from_ptr(json) };
+ let str = c_str.to_str().unwrap();
+
+ dbg!(str);
+
+ // log::info!("rust_insert_filecache_from_go: {:#?}", str);
+
+ // let mut fc: FileCache = serde_json::from_str(str).unwrap();
+
+ // log::info!("rust_insert_filecache_from_go fc: {:#?}", fc);
+
+ // let slice = unsafe { std::slice::from_raw_parts(buf, len as usize) };
+ // fc.bytes = Some(slice.to_vec());
+
+ // fc.insert().unwrap();
+}
+
 pub fn main() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
- let data = include_bytes!("/Users/e/Desktop/ep2.mp4");
- let mut offset: usize = 0;
- loop {
-  let (_, b) = Box::parse(&data[offset..])?;
-  dbg!(&b);
-  offset += b.size as usize;
+ let cstring = CString::new("".to_owned()).unwrap();
+
+ unsafe {
+  GoListJSON(cstring.as_ptr());
  }
+
+ // let data = include_bytes!("/Users/e/Desktop/ep2.mp4");
+ // let mut offset: usize = 0;
+ // loop {
+ //  let (_, b) = Box::parse(&data[offset..])?;
+ //  dbg!(&b);
+ //  offset += b.size as usize;
+ // }
+
+ Ok(())
 }
